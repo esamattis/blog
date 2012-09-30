4d81bca49489d94e909296f6783dc56c4a4d7e56
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-09-30 22:34:17 +0300

    Add notify pluging

---
+++ plugins/notify.rb
@@ -0,0 +1,26 @@
+
+
+require "colorize"
+
+module Jekyll
+
+  class Site
+
+    alias orig_process process
+    def process
+
+      @preview_mode = ENV.has_key?('OCTOPRESS_ENV') && ENV['OCTOPRESS_ENV'] == 'preview'
+
+      if @preview_mode
+        puts "Generating for preview...".colorize( :red )
+      else
+        puts "Generating for production...".colorize( :green )
+      end
+
+      start = Time.now
+      orig_process
+      puts "Generation done. Took #{ Time.now - start } seconds".colorize( :green )
+    end
+
+  end
+end
